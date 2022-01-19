9d1152f431a651cca9013838beac7c467d0ff5d2
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-19 10:29:06 +0530

    Update staticweb.tf

--- staticweb.tf
+++ staticweb.tf
@@ -3,7 +3,7 @@ region = "us-east-2"
 }
 resource "aws_amplify_app" "simple-static-website" {
   name       = "simple-static-website"
-  repository = "https://github.com/Ramana4u/simple-static-website.git"
+  repository = "https://github.com/vigneshs123123/static-website-example.git"
   access_token = "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"
 
   # The default build_spec added by the Amplify Console for React.
